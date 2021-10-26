6332a79582461d3d89b6fddf268ad8e823d5e2ae
Albert <albertmudryk715567@gmail.com>
2021-10-26 18:05:05 +0300

    script.js first prog v 1

--- js/script.js
+++ js/script.js
@@ -10,11 +10,32 @@
 // вся информация от пользователя - це тип string
 // з допомогою 5 + +"3" з строки можна зробити число
 
-const answers = [];
+//const answers = [];
 
-answers[0] = prompt('Как ваше имя?');
-answers[1] = prompt('Как ваше фамилия?');
-answers[2] = prompt('Сколько вам лет?');
+//answers[0] = prompt('Как ваше имя?');
+//answers[1] = prompt('Как ваше фамилия?');
+//answers[2] = prompt('Сколько вам лет?');
 
-alert(`Привет, ${answers[0]} ${answers[1]}. Тебе ${answers[2]} лет`);
+//alert(`Привет, ${answers[0]} ${answers[1]}. Тебе ${answers[2]} лет`);
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+console.log(personalMovieDB.count);
+
+const a = prompt('Один из последних просмотрених фильмов', '');
+const b = prompt('Оценка фильма', '');
+const c = prompt('Один из последних просмотрених фильмов', '');
+const d = prompt('Оценка фильма', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
