4d054ea7b54835033771605c30d74f1d7f878db0
Albert <albertmudryk715567@gmail.com>
2021-10-26 19:11:28 +0300

    script.js first prog v 2

--- js/script.js
+++ js/script.js
@@ -19,7 +19,6 @@
 //alert(`Привет, ${answers[0]} ${answers[1]}. Тебе ${answers[2]} лет`);
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
-console.log(numberOfFilms);
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -28,14 +27,55 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-console.log(personalMovieDB.count);
 
-const a = prompt('Один из последних просмотрених фильмов', '');
-const b = prompt('Оценка фильма', '');
-const c = prompt('Один из последних просмотрених фильмов', '');
-const d = prompt('Оценка фильма', '');
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    alert('Вы класическй зритель');
+} else if (personalMovieDB.count >= 30){
+    alert('Вы кономан');
+} else {
+    alert('Произошла ошибка');
+}
+let i = 0;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let j = 0; j < 2; j++){
+    let a = prompt('Один из последних просмотрених фильмов', '');
+    let b = prompt('Оценка фильма', '');
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }  
+}
+do{
+    let a = prompt('Один из последних просмотрених фильмов', '');
+    let b = prompt('Оценка фильма', '');
+    i++;
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }  
+}
+while(i < 2);
+
+
+while(i < 2){
+    let a = prompt('Один из последних просмотрених фильмов', '');
+    let b = prompt('Оценка фильма', '');
+    i++;
+    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }  
+}
 
 console.log(personalMovieDB);
