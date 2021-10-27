766bdc80c9681cfadc9d4e106f2d7f7303740c5c
Albert <albertmudryk715567@gmail.com>
2021-10-27 20:20:52 +0300

    porg v4, script add copy..

--- js/script.js
+++ js/script.js
@@ -312,3 +312,26 @@ const q = {
 };
 
 const newObj = {...q};
+
+//                                  OOP, Prototype
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
+
+// const john = {
+//     health: 100
+// };
+
+const john = Object.create(soldier);
+
+// john.__proto__ = soldier;
+// Object.setPrototypeOf(john, soldier);
+
+
+// console.log(john.sayHello);
+john.sayHello();
