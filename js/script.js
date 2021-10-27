e5840f53c4e31cf6e2aad2cd49fccae567bcb4fd
Albert <albertmudryk715567@gmail.com>
2021-10-27 10:36:48 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -18,64 +18,88 @@
 
 //alert(`Привет, ${answers[0]} ${answers[1]}. Тебе ${answers[2]} лет`);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// if (personalMovieDB.count < 10) {
+//     alert('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+//     alert('Вы класическй зритель');
+// } else if (personalMovieDB.count >= 30){
+//     alert('Вы кономан');
+// } else {
+//     alert('Произошла ошибка');
+// }
+// let i = 0;
+
+// for(let j = 0; j < 2; j++){
+//     let a = prompt('Один из последних просмотрених фильмов', '');
+//     let b = prompt('Оценка фильма', '');
+//     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+// }
+// do{
+//     let a = prompt('Один из последних просмотрених фильмов', '');
+//     let b = prompt('Оценка фильма', '');
+//     i++;
+//     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+// }
+// while(i < 2);
+
+
+// while(i < 2){
+//     let a = prompt('Один из последних просмотрених фильмов', '');
+//     let b = prompt('Оценка фильма', '');
+//     i++;
+//     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }  
+// }
+
+// console.log(personalMovieDB);
+
+getModule(10, 1);
+
+function getModule(x, y){
+    return console.log(x + y);
+}
+//function declaretion - можна обявлять перед созданием
+
+
+let fun = function(){
+    return 10 + 2;
+};
+//function expression - можна обявлять после созданием
+console.log(fun());
+
+const calc = a => a * a;
+console.log(calc(12));
+
+const calcul = (a, b) => {
+    console.log('1');
+    return console.log((a * (b + a)));
 };
 
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    alert('Вы класическй зритель');
-} else if (personalMovieDB.count >= 30){
-    alert('Вы кономан');
-} else {
-    alert('Произошла ошибка');
-}
-let i = 0;
-
-for(let j = 0; j < 2; j++){
-    let a = prompt('Один из последних просмотрених фильмов', '');
-    let b = prompt('Оценка фильма', '');
-    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }  
-}
-do{
-    let a = prompt('Один из последних просмотрених фильмов', '');
-    let b = prompt('Оценка фильма', '');
-    i++;
-    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }  
-}
-while(i < 2);
-
-
-while(i < 2){
-    let a = prompt('Один из последних просмотрених фильмов', '');
-    let b = prompt('Оценка фильма', '');
-    i++;
-    if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--;
-    }  
-}
-
-console.log(personalMovieDB);
+calcul(4, 6);
